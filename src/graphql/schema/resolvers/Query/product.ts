81a897dfb73769534818830098227833623d2d09
patrykharasiuk <patryk.harasiuk@op.pl>
2023-09-24 21:04:04 +0200

    feat: add prisma schema

--- src/graphql/schema/resolvers/Query/product.ts
+++ src/graphql/schema/resolvers/Query/product.ts
@@ -1,8 +1,21 @@
 import type { QueryResolvers } from "./../../../types.generated";
+import { faker } from "@faker-js/faker";
+
 export const product: NonNullable<QueryResolvers["product"]> = async (
   _parent,
-  _arg,
-  _ctx,
+  arg,
+  _ctx
 ) => {
-  /* Implement Query.product resolver logic here */
+  const name = faker.commerce.productName();
+  const slug = faker.helpers.slugify(name);
+  const price = faker.number.int({ min: 100, max: 10000 });
+  const description = faker.commerce.productDescription();
+
+  return {
+    id: arg.id,
+    name,
+    slug,
+    price,
+    description,
+  };
 };
