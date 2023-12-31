ba36f4f6a80300635dc906d1c2d33e9071ee558f
patrykharasiuk <patryk.harasiuk@op.pl>
2023-12-31 18:06:31 +0100

    feat: add addToOrder to order data_access dir

--- src/data_access/order.ts
+++ src/data_access/order.ts
@@ -1,30 +1,17 @@
-// import type { QueryResolvers } from "./../../../types.generated";
+// extend type Query {
+// 	order(id: ID!): Order
+// }
 
-// export const order: NonNullable<QueryResolvers["order"]> = async (
-// 	_parent,
-// 	arg,
-// 	ctx,
-// ) => {
-// 	/* Implement Query.order resolver logic here */
-// 	const orderId = arg.id;
-// 	const order = await ctx.prisma.order.findUnique({
-// 		where: {
-// 			id: orderId,
-// 		},
-// 		include: {
-// 			orderItems: {
-// 				include: {
-// 					product: true,
-// 				},
-// 			},
-// 		},
-// 	});
-// 	if (!order) return null;
-// 	return order as any;
-// };
+// type Mutation {
+// 	addToOrder(orderId: ID, productId: ID!, quantity: Int!): Order
+// 	removeOrderItem(orderId: ID!, orderItemId: ID!): Order
+// 	removeOrderItems(orderId: ID!): Order
+// 	updateOrderItemQuantity(orderId: ID!, orderItemId: ID!): Order
+// }
 
 import type { PrismaClient, Order } from "@prisma/client";
 import { db } from "../utils/db";
+import { products } from "./product";
 
 const Orders = (prismaOrders: PrismaClient["order"]) => {
 	return Object.assign(prismaOrders, {
@@ -43,6 +30,72 @@ const Orders = (prismaOrders: PrismaClient["order"]) => {
 			if (!order) throw new Error(`Order with id ${id} does not exist`);
 
 			return order;
+		},
+
+		async add(productId: string, quantity: number, orderId?: string) {
+			const product = await products.getById(productId);
+
+			if (!product)
+				throw new Error(`Product with ${productId} id does not exist`);
+
+			if (!orderId) {
+				const newOrder = await db.order.create({
+					data: {
+						status: "pending",
+
+						orderItems: {
+							create: {
+								quantity,
+								product: {
+									connect: {
+										id: productId,
+									},
+								},
+							},
+						},
+					},
+
+					include: {
+						orderItems: {
+							include: { product: { include: { categories: true } } },
+						},
+					},
+				});
+
+				return newOrder;
+			}
+
+			const currentOrder = await db.order.update({
+				where: {
+					id: orderId,
+				},
+				data: {
+					orderItems: {
+						upsert: {
+							where: {
+								orderId_productId: {
+									orderId,
+									productId,
+								},
+							},
+							create: {
+								quantity,
+								productId,
+							},
+							update: {
+								quantity: { increment: quantity },
+							},
+						},
+					},
+				},
+				include: {
+					orderItems: {
+						include: { product: { include: { categories: true } } },
+					},
+				},
+			});
+
+			return currentOrder;
 		},
 	});
 };
