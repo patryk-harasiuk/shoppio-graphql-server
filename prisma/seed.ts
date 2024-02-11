3ce3b3359e02b2362487e56469f0e2a7819c976f
Patryk Harasiuk <patryk.harasiuk@op.pl>
2024-02-11 01:17:18 +0100

    update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
@@ -14,8 +14,7 @@ const createCategoryAndProducts = async (
 	for (let i = 0; i < productsCount; i++) {
 		const name = faker.commerce.productName();
 		const description = faker.commerce.productDescription();
-		const price = Number(faker.commerce.price()) * 100;
-		// const price = new Prisma.Decimal(faker.commerce.price())
+		const price = new Prisma.Decimal(Number(faker.commerce.price()) * 100);
 
 		const data = { name, description, price };
 
